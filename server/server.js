5a6b5fb1913efaee2071172623d9a8927a9bb4fb
MD Toufique <90861203+mdtoufique@users.noreply.github.com>
2024-12-20 19:37:03 +0600

    port updated

--- server/server.js
+++ server/server.js
@@ -318,6 +318,10 @@ app.get("/pcserver", (req, res) => {
 });
 
 // Start the server on a specified port
-server.listen(3000, () => {
-	console.log("Server is running on port 3000");
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
+// server.listen(3000, () => {
+// 	console.log("Server is running on port 3000");
+// });
