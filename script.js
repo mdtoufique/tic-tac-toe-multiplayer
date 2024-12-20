dbf5501aaac857bcefc6356854824181eed7e1e6
MD Toufique <90861203+mdtoufique@users.noreply.github.com>
2024-12-20 20:37:14 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 import { io, Socket } from "socket.io-client";
-
+console.log("hi 1");
 const socket = io("http://192.168.0.107:3000");
 
-
+console.log("hi 2");
 document.getElementById("loading").style.display = "none";
 updateFormVisibility(false);
 
